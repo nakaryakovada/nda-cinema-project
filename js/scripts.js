0a47411714ba006986c6e42c028dd57cc2bfe2db
nakaryakovada <114469085+nakaryakovada@users.noreply.github.com>
2022-10-04 22:43:12 +0500

    Верстка закончена

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+const schemeSvg = document.querySelector(".scheme-svg");
+const totalPriceTag = document.querySelector(".price-total");
+const menuButton = document.querySelector(".m-menu");
+const menu = document.querySelector(".menu");
+
+let cost = 500;
+let totalPrice = 0;
+
+schemeSvg.addEventListener("click", (event) => {
+  if (!event.target.classList.contains("booked"))
+    event.target.classList.toggle("active");
+  let totalSeats = schemeSvg.querySelectorAll(".active").length;
+  totalPrice = cost * totalSeats;
+  totalPriceTag.textContent = totalPrice;
+});
+
+menuButton.addEventListener('click', () => {
+  console.log("Клик по меню");
+  menu.classList.toggle("is-open");
+});
